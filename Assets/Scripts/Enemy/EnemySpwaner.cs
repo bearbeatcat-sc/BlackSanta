269428517c456ca873902f9e1ad60f88209e7d9c
Shingo Inaba <bearbeatcat@gmail.com>
2024-01-10 00:49:26 +0900

    sabun

--- Assets/Scripts/Enemy/EnemySpwaner.cs
+++ Assets/Scripts/Enemy/EnemySpwaner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98b92994ec5b1e0b456e1bfecaed689f067372f00024c5470806afee0f635dc7
-size 2130
+oid sha256:cdf4756eff442d0bb0d18f975d0829f3263b97e18633bc6b61a17e39c08aa1a5
+size 2207
