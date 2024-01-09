a46309db435bfd19fd9114b559643094e574697d
Shingo Inaba <bearbeatcat@gmail.com>
2024-01-09 22:45:28 +0900

    0でもLVUP出来るバグの修正

--- Assets/Scripts/Player/PlayerExpSystem.cs
+++ Assets/Scripts/Player/PlayerExpSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f264fc733482c7c9b67647b115ff6cac89444d9f2103cc583de0f16caca43d8e
-size 2591
+oid sha256:5c03735704994cc18b0e5c751a012dab46e86264120297dd5bcd9747883697b8
+size 2652
