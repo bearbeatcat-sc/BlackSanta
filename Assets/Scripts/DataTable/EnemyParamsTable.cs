b9bf3f4dda2959ef369a3b48dcab95dff22f7d22
Shingo Inaba <bearbeatcat@gmail.com>
2024-01-09 23:25:51 +0900

    車エネミーの実装

--- Assets/Scripts/DataTable/EnemyParamsTable.cs
+++ Assets/Scripts/DataTable/EnemyParamsTable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:228b943fede2ef542ff6b145631867e0746c0ddece81a6c10ac460004346b71f
-size 1863
+oid sha256:abd83e0c796e5232230bd1070b868d8a165778ee28091a57e6fbb38950070e3e
+size 3703
---
+++ Assets/Scripts/Enemy/CarEnemey.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b88f175a6447f2bb2971d622866160ecd70535406ef90dd742609a3ff72788e
+size 2361
