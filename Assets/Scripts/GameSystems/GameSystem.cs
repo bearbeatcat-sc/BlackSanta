b66cf0beac327be2f9ccd968578064ac28de8b89
Shingo Inaba <bearbeatcat@gmail.com>
2024-01-09 22:00:14 +0900

    サウンドの追加

--- Assets/Scripts/GameSystems/GameSystem.cs
+++ Assets/Scripts/GameSystems/GameSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca53c4cc2a1d2bbcb377165540fdfe1017015e8c4d47fcae3293acb37d233228
-size 14190
+oid sha256:48be831ce10355e3daf57fb7fac7d225c198055cef3f59163c1c27519b9b7f63
+size 14855
---
+++ Assets/Scripts/GameSystems/SoundSystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9bbf3948032ff0e3efd257fed3a06ea5a02fbe43f0071f28edc8e8d245d04e8
+size 683
