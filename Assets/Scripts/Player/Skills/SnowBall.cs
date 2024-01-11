7142dce30f65555ceed60582af772809fcbdbe48
Shingo Inaba <bearbeatcat@gmail.com>
2024-01-12 01:02:29 +0900

    雪玉のダメージが入らない不具合の修正

--- Assets/Scripts/Player/Skills/SnowBall.cs
+++ Assets/Scripts/Player/Skills/SnowBall.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44f9e66d64fdd823ec701a61c5e1417126e2db3151864b62846749aa2a9c077d
-size 5156
+oid sha256:82071c2587de2b6ac3e97bb9356f8f46d233d80a51d5dc04ec99171b674454d5
+size 5141
