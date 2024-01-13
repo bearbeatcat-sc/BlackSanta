368138fd0baa8d5091d65bbc258cb2b6f27d37fd
Shingo Inaba <bearbeatcat@gmail.com>
2024-01-13 17:53:57 +0900

    ゲーム終了後の音量の調整

--- Assets/Scripts/GameSystems/GameSystem.cs
+++ Assets/Scripts/GameSystems/GameSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd4e98d7bead0907305b232e1f06954f1ce6cb2f025b75e059b4c7a25423328
-size 16439
+oid sha256:29a164f9eb7a07df4c9b54912c59e89821ffb34910dc4fc5315efda32cea1140
+size 16498
